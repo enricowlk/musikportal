caf7935b81a2eac067a021a896568507ba2d8c24
enricowlk <enrico.woelck@gmail.com>
2025-08-05 20:20:52 +0200

    logo opacity angepasst

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,11 +11,10 @@ export default async function RootLayout({
     <html lang="de" className="h-full" suppressHydrationWarning>
       <body className="h-full transition-colors duration-200">
         <img
-    src="/Logo/dtvlogoerweitert.png"
-    alt="DTV Logo"
-    className="fixed top-1/2 right-0 -translate-y-1/2 w-[60vw] max-w-none opacity-30 pointer-events-none z-0 dark:opacity-20"
-  />
-
+          src="/Logo/dtvlogoerweitert.png"
+          alt="DTV Logo"
+          className="fixed top-1/2 right-0 -translate-y-1/2 w-[60vw] max-w-none pointer-events-none z-0 dark:opacity-80"
+        />
         <ThemeProvider>
           {children}
         </ThemeProvider>
